4037552335370f3dfe7af96dfca6173ba514a49d
Florian-eart <lecareuxflorian@gmail.com>
2022-02-07 16:08:02 +0100

    MAJ Main

--- Assets/Scripts/Character/CharacterRenderer.cs
+++ Assets/Scripts/Character/CharacterRenderer.cs
@@ -9,6 +9,7 @@ public class CharacterRenderer : MonoBehaviour
     [SerializeField] private Animator _playerAnimator;
     [SerializeField] private CinemachineVirtualCamera[] _cinemachineCameras;
     public PlayableDirector _playbleDirector;
+    public GameObject _canvas, _rythmBar;
 
     // private void Start() 
     // {
@@ -19,6 +20,8 @@ public class CharacterRenderer : MonoBehaviour
     {
         _playerAnimator.SetBool("_run", true);
         _playbleDirector.Play();
+        _canvas.SetActive(true);
+        _rythmBar.SetActive(true);
     }
 
     public void SetJumpBool(bool isJumping)
